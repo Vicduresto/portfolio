bc676733fc98eab7789e4fbd22ec07b92ca3b4b4
Vicduresto <vicduresto@gmail.com>
2024-10-03 17:17:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
     const body = document.body;
 
-    if (scrollPosition > 0) {
+    // Déclenche la transition après 200 pixels de défilement
+    if (scrollPosition > 200) {
         body.classList.add('scrolled');
     } else {
         body.classList.remove('scrolled');
